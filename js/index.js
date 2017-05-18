0a6d856900ab1ccd203fb27fe1661c55a4e77aea
wujunxing <jmwujunxing@gmail.com>
2017-05-18 23:33:00 +0800

    view

---
+++ js/index.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $("#floatimg").adFloat({ width: 252, height: 172, top: 0, left: 0, step: 2 });
+    $("#closead").click(function () {
+        $("#floatimg").remove();
+    });
+});
