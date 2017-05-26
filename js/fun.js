720162e366e9210016fff3419af185442943c673
wujunxing <jmwujunxing@gmail.com>
2017-05-27 00:04:06 +0800

    5

--- js/fun.js
+++ js/fun.js
@@ -1,18 +1,35 @@
 ﻿$(function () {
+    /*主要导航菜单*/
+    $("#nav_nw .li01").hover(function () {
+        $(this).find(".ul02").stop().slideDown(400);
+    }, function () {
+        $(this).find(".ul02").stop().slideUp(0);
+    });
 
+    $("#nav_nw .li02").hover(function () {
+        $(this).find(".ul03").stop().slideDown(400);
+        $(this).addClass("link02_hover");
+    }, function () {
+        $(this).find(".ul03").stop().slideUp(0);
+        $(this).removeClass("link02_hover");
+    });
+})
+
+$(function () {
+    /*banner*/
     if ($("#slider").find("img").length > 1) {
         $("#slider").nivoSlider({ pauseTime: 3000, controlNav: false, controlNavThumbs: true });
     }
     else {
         $("#slider").removeClass("nivoSlider");
     }
-
+    /*droplinks*/
     $(".bbs_nav .links01").hover(function () {
         $(this).children(".slist").show()
     }, function () {
         $(this).children(".slist").hide()
     })
-
+    /*css*/
     $(".bbs_list").find(".item:odd").addClass("split");
     $(".bbs_list").find(".item:last").addClass("last");
 });
